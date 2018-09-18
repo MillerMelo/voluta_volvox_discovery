0f41e59fe718ad92071b868eedfafbf226217cd9
Miller Melo <milleralfonso.m@gmail.com>
2018-09-17 23:05:46 -0500

    Add ADC Temp & Vbatt

--- main_volvox.py
+++ main_volvox.py
@@ -1,10 +1,12 @@
-import pyb
+#import pyb
 import time
-from pyb import Pin, Timer, ExtInt
+from pyb import Pin, Timer, ExtInt, ADCAll
 import motor_wheel
 
 
 #out_m5_cw = Pin('PA5', Pin.OUT_PP)
+
+adcall = ADCAll(12, 0x70000) # 12 bit resolution, internal channels On
 
 in_btn_play 	= Pin('PB12', Pin.IN)
 in_btn_home 	= Pin('PB13', Pin.IN)
@@ -20,11 +22,17 @@ out_enable_x	= Pin('PE4', Pin.OUT_PP)
 out_enable_y	= Pin('PE5', Pin.OUT_PP)
 out_enable_z	= Pin('PE6', Pin.OUT_PP)
 
-conf_fl_srv_vel = (15.0, 95.0, 14.0, 91.0)
-conf_fr_srv_vel = (15.0, 95.0, 14.0, 91.0)
-conf_bl_srv_vel = (15.0, 95.0, 14.0, 91.0)
-conf_br_srv_vel = (15.0, 95.0, 14.0, 91.0)
 
+
+
+
+conf_fl_srv_vel = (15.0, 85.0, 14.0, 81.0)
+conf_fr_srv_vel = (15.0, 85.0, 14.0, 81.0)
+conf_bl_srv_vel = (15.0, 85.0, 14.0, 81.0)
+conf_br_srv_vel = (15.0, 85.0, 14.0, 81.0)
+
+adc_temp = 0.0
+adc_vbatt = 0.0
 
 #timer_2 = Timer(2, freq=1000)
 
@@ -34,24 +42,33 @@ def move(pct_vel):
 	motor_bl.run_scale(pct_vel)
 	motor_br.run_scale(pct_vel)
 
-#Motor Front Left
+	#pot_value = int(pot_pin_adc.read())
+
+
+
+
+#Config Motor Front Left
 motor_fl = motor_wheel.MotorWheel('PA0', 5, 1, 'PB6', 4, 1)	
 motor_fl.update_config(conf_fl_srv_vel)
 
-#Motor Front Right
+#Config Motor Front Right
 motor_fr = motor_wheel.MotorWheel('PA1', 5, 2, 'PB7', 4, 2)	
 motor_fr.update_config(conf_fr_srv_vel)
 
-#Motor Back Left
+#Config Motor Back Left
 motor_bl = motor_wheel.MotorWheel('PA2', 5, 3, 'PB8', 4, 3)	
 motor_bl.update_config(conf_bl_srv_vel)
 
-#Motor Back Right
+#Config Motor Back Right
 motor_br = motor_wheel.MotorWheel('PA3', 5, 4, 'PB9', 4, 4)	
 motor_br.update_config(conf_br_srv_vel)
 
 
 while True:
+
+	adc_temp = adcall.read_core_temp()
+	adc_vbatt = adcall.read_core_vbat()
+	print("%.2f V %.2f °C" %(adc_vbatt, adc_temp))
 
 	for vel_temp in range (0, 100, 1):
 		move(vel_temp)
@@ -74,4 +91,6 @@ while True:
 
 	move(0)
 	time.sleep(2)
+	
+
 	
